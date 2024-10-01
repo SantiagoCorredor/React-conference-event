a0f5802c788939f45e53fafca0293d7d97f64f92
SantiagoCorredor <santiagocorredor9@gmail.com>
2024-10-01 18:50:47 -0400

    Whole project

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
         {
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2014/08/26/21/59/projector-428664_1280.jpg",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2018/10/10/13/59/isolated-3737329_1280.jpg",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2016/11/21/11/32/audio-1844798_1280.jpg",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/10/30/19/24/whiteboard-2903269_1280.png",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/08/06/21/47/signage-2596694_1280.jpg",
         name: "Signage",
         cost: 80,
         quantity: 0,
--- src/venueSlice.js
+++ src/venueSlice.js
@@ -5,31 +5,31 @@ export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2018/06/28/11/47/bank-3503689_1280.jpg",
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2016/08/16/09/53/international-conference-1597531_1280.jpg",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2023/04/26/08/07/chairs-7951845_1280.jpg",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2017/03/28/12/16/chairs-2181980_1280.jpg",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://cdn.pixabay.com/photo/2013/03/07/15/37/building-91228_1280.jpg",
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
